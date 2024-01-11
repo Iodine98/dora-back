0cb5850910d5adddbf578c87bc6003c409918a17
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2024-01-11 12:14:51 +0000

    Refactor RetrievalQA to ConversationalRetrievalChain

--- server.py
+++ server.py
@@ -153,7 +153,7 @@ def prompt() -> Response:
     message = request.form["prompt"]
     chatbot = Chatbot(
         user_id=session["sessionId"],
-        document_dict=session["files"],
+        # document_dict=session["files"],
     )
     prompt_response = PromptResponse(
         message="Prompt result is found under the result key.", error="", result=chatbot.send_prompt(message)
