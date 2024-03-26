ea045d889fa401a68536f575098bfe0d52df6f2c
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2024-03-26 16:07:02 +0100

    Update III

--- server_modules/models.py
+++ server_modules/models.py
@@ -82,8 +82,9 @@ def update_record_with_answers(
         Utils.get_env_variable("FINAL_ANSWER_CONNECTION_STRING")
     )
     logger.info(f"Updating final answer for session_id: {session_id}")
+    current_session_id = sqlalchemy.bindparam("session_id_param", value=session_id)
     answer_model_record_query = sqlalchemy.select(FinalAnswerModel).where(
-        FinalAnswerModel.session_id == str(session_id)
+        FinalAnswerModel.session_id == current_session_id
     )  # SELECT * FROM final_answer WHERE session_id = session_id
     update_stmt = (
         sqlalchemy.update(FinalAnswerModel)
@@ -95,12 +96,11 @@ def update_record_with_answers(
         )
     )  # UPDATE final_answer SET original_answer = original_answer, edited_answer = edited_answer, end_time = NOW() WHERE session_id = session_id
     with db_final_answer_engine.connect() as connection:
-        temp_var = sqlalchemy.select(FinalAnswerModel).where(
-            FinalAnswerModel.session_id == "b08a1734-555c-4f6f-ae5c-d96749ff748b"
-        )
-        logger.info(f"Executing query: {str(temp_var)}")
-        answer_model_record = connection.execute(temp_var).all()
-        logger.info(f"Answer model record: {answer_model_record}")
+        logger.info(f"current session id: {current_session_id}")
+        logger.info(f"type of session id: {type(current_session_id)}")
+        logger.info(f"Executing query: {str(answer_model_record_query)}")
+        answer_model_record = connection.execute(answer_model_record_query).fetchone()
+        logger.info(f"Answer model record: {answer_model_record} vs {session_id}")
         # if not answer_model_record.fetchone():
         # raise ValueError(f"No record found for session_id: {session_id}")
     #     connection.execute(update_stmt)
